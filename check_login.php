6cf85a6fb118d1d6cc91ca8787d9a8ba577bb79e
tueseed <tue_seed@hotmail.com>
2018-04-19 21:31:48 +0700

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -3,10 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Surwayer Web App</title>
-<link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
-<link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
-<script src="jquery-1.6.4.min.js" type="text/javascript"></script>
-<script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>
+
 </head> 
 <body> 
 <?php
@@ -25,17 +22,6 @@
 	echo $U;
 	?>
 	
-<div data-role="page" id="page">
-	<div data-role="header">
-		<h1>Surwayer Web App</h1>
-	</div>
-	
-	<div data-role="footer">
-		<h4>Dev By Tueseed</h4>
-	</div>
-</div>
-
-
 
 </body>
 </html>
