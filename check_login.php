7ddb834611f2026e82a819a5775fe3417491dd99
tueseed <tue_seed@hotmail.com>
2018-04-19 21:30:03 +0700

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -1,14 +1,20 @@
+<!DOCTYPE html> 
 <html>
 <head>
-<title>BMS:ระบบบริหารงบทำการ</title>
-</head>
-<body>
+<meta charset="utf-8">
+<title>Surwayer Web App</title>
+<link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
+<link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
+<script src="jquery-1.6.4.min.js" type="text/javascript"></script>
+<script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>
+</head> 
+<body> 
 <?php
 	require('./connect-db.php');
-	//session_start();
-	//$U = $_POST['textuser'];
-	//$P = $_POST['textpass'];
-$r = "454123156231654";
+	session_start();
+	$U = $_POST['textuser'];
+	$P = $_POST['textpass'];
+
 		//$sql = "SELECT * FROM tbl_staff";
 		
 		//$query_type = mysqli_query($conn,$sql_type);
@@ -16,7 +22,20 @@ $r = "454123156231654";
 		//while($ofname = mysqli_fetch_array($query)){ 
 		//	$ofname1 = $ofname["office_name"];
 		//}
-	echo ";nmk;nnkjn";
+	echo $U;
 	?>
+	
+<div data-role="page" id="page">
+	<div data-role="header">
+		<h1>Surwayer Web App</h1>
+	</div>
+	
+	<div data-role="footer">
+		<h4>Dev By Tueseed</h4>
+	</div>
+</div>
+
+
+
 </body>
 </html>
