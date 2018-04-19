73574b3c7aad4d0a6cd8fc40b4c0461394503286
tueseed <tue_seed@hotmail.com>
2018-04-19 21:03:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,66 +8,15 @@
 <script src="jquery-1.6.4.min.js" type="text/javascript"></script>
 <script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>
 </head> 
-	<?php
+	
+<body> 
+<?php
 		require('./connect-db.php');
 		$sql = "SELECT * FROM tbl_staff";
 		$query = mysqli_query($conn,$sql);
 		$objResult = mysqli_fetch_array($query);
 		echo $objResult."13218123161";
 	?>
-<body> 
-
-<div data-role="page" id="page">
-	<div data-role="header">
-		<h1>Page One</h1>
-	</div>
-	<div data-role="content">	
-		<ul data-role="listview">
-			<li><a href="#page2">Page Two</a></li>
-            <li><a href="#page3">Page Three</a></li>
-			<li><a href="#page4">Page Four</a></li>
-		</ul>		
-	</div>
-	<div data-role="footer">
-		<h4>Page Footer</h4>
-	</div>
-</div>
-
-<div data-role="page" id="page2">
-	<div data-role="header">
-		<h1>Page Two</h1>
-	</div>
-	<div data-role="content">	
-		Content		
-	</div>
-	<div data-role="footer">
-		<h4>Page Footer</h4>
-	</div>
-</div>
-
-<div data-role="page" id="page3">
-	<div data-role="header">
-		<h1>Page Three</h1>
-	</div>
-	<div data-role="content">	
-		Content		
-	</div>
-	<div data-role="footer">
-		<h4>Page Footer</h4>
-	</div>
-</div>
-
-<div data-role="page" id="page4">
-	<div data-role="header">
-		<h1>Page Four</h1>
-	</div>
-	<div data-role="content">	
-		Content		
-	</div>
-	<div data-role="footer">
-		<h4>Page Footer</h4>
-	</div>
-</div>
 
 </body>
 </html>
