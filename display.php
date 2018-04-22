093b6b231c2b55276730c094a6d9b73c43d4b130
tueseed <tue_seed@hotmail.com>
2018-04-22 18:06:16 +0700

    Add files via upload

---
+++ display.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+	<?php
+		require('./connect-db.php');
+		$sql_select = "SELECT * FROM tbl_jobmeter";
+		$resual = mysqli_query($conn,$sql_select);
+	?>
+</head>
+	<?php
+		while($row = mysqli_fetch_array($result))
+			{
+				echo "<tr align='center'>";
+				echo "<td>".$row['name']."</td>";
+				echo "</tr>";
+			
+			
+			
+			
+			}
+	?>
+<body>
+</body>
+</html>
