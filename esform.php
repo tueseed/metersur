ac19c9545681ae1a1e6a28e482a09d386e5fb86c
tueseed <tue_seed@hotmail.com>
2018-04-23 09:18:10 +0700

    Add files via upload

---
+++ esform.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+<?php
+$cs_reqnumber = $_GET['cs_reqnumber'];
+echo $cs_reqnumber;
+?>
+</body>
+</html>
