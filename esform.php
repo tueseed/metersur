86636accd2a35464a9212616dd856a120b26503f
tueseed <tue_seed@hotmail.com>
2018-04-23 11:06:07 +0700

    Add files via upload

--- esform.php
+++ esform.php
@@ -2,12 +2,16 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>SDCI-Surwayors|Designer|Constructor|Inspector</title>
 </head>
 <body>
 <?php
 $cs_reqnumber = $_GET['cs_reqnumber'];
-echo $cs_reqnumber;
+require('./connect-db.php');
+		$sql_search = "SELECT * FROM tbl_jobmeter";
+		$result = mysqli_query($conn,$sql_search);
+		$objectresult = mysqli_fetch_array($result);
+echo $objectresult["cs_name"];
 ?>
 </body>
 </html>
