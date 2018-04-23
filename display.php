7ff8c1c24b4b3fbdb626186709f096935c0e3c01
tueseed <tue_seed@hotmail.com>
2018-04-23 08:46:16 +0700

    Add files via upload

--- display.php
+++ display.php
@@ -2,7 +2,15 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Untitled Document</title>
+<title>SDCI-Surwayors|Designer|Constructor|Inspector</title>
+<script language="javascript">
+function OpenPopup(NUMBER)
+	{
+		var left = (screen.width/2)-(1000/2);
+        var top = (screen.height/2)-(300/2);
+		window.open('show.php?NUMBER='+NUMBER,'myPopup','width=1000,height=300,toolbar=0, menubar=0,location=0,status=1,scrollbars=1,resizable=1,left='+left+',top='+top);
+	}
+</script>
 	<?php
 		require('./connect-db.php');
 		$sql_select = "SELECT * FROM tbl_jobmeter";
@@ -19,6 +27,7 @@
 			{
 				echo "<tr align='center'>";
 				echo "<td>".$row["cs_name"]."</td>";
+				echo "<td><input type='button' name='button' id='button' value='...' onclick='OpenPopup(".$row["cs_reqnumber"].")'></td> ";
 				echo "</tr>";
 			
 			
