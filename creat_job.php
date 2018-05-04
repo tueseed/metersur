6a175c793ed11f79869df726d34112c6294582be
tueseed <tue_seed@hotmail.com>
2018-05-04 16:38:11 +0700

    Add files via upload

---
+++ creat_job.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<script src="./jquery-3.3.1.min.js"></script>
+</head>
+<body>
+<h2>ลงทะเบียนผู้จัดการ</h2>
+</body>
+</html>
