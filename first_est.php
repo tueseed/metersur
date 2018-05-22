1c1cb8c6a28c52236b364ce9c6639b7963d2ee16
tueseed <tue_seed@hotmail.com>
2018-05-22 10:52:00 +0700

    Add files via upload

--- first_est.php
+++ first_est.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html> 
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1" data-ajax="false" charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0," data-ajax="false" charset="utf-8">
 <title>Surwayer Web App</title>
 <link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
 <link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
@@ -9,26 +9,21 @@
 <script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>
 </head> 
 <body> 
-
 <div data-role="page" id="page">
 	<div data-role="header">
-		<h1>Surwayer Web App</h1>
+		<h1>ประมาณการค่าใช้จ่าย</h1>
 	</div>
-	<div data-role="content">
-	 	<form name="form1" action="check_login.php" method="post">
-	    <label for="textinput">ชื่อผู้ใช้งาน:</label>
-	    <input type="text" name="textuser" id="textuser" value=""  />
-     	<label for="textinput">รหัสผ่าน:</label>
-	    <input type="text" name="textpass" id="textpass" value=""  />
-	    <input type="submit" value="เข้าสู่ระบบ">
-        </form>
-	</div>
-	<div data-role="footer">
-		<h4>Dev By Tueseed</h4>
-	</div>
+    <div data-role="content"> 
+        <fieldset data-role="controlgroup">
+            <legend class="txt-in">เสา:</legend>
+            <select name="phase" id="phase" onchange="select_head_ht(this.value)">
+                <option value="N/A">เฟส</option>
+                <option value="1">1 เฟส</option>
+                <option value="3">3 เฟส</option>
+            </select><!--เฟส-->
+        </fieldset>
+    </div>   
 </div>
-
-
 
 </body>
 </html>
