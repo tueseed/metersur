7b52db1bdb4fdba407ea19fc9d6881fa35fdafc9
tueseed <tue_seed@hotmail.com>
2018-05-22 10:41:00 +0700

    Update first_est.php

--- first_est.php
+++ first_est.php
@@ -3,11 +3,10 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0," data-ajax="false" charset="utf-8">
 <title>Surwayer Web App</title>
-<link rel="stylesheet" href="icons.css">
 <link href="jquery.mobile.theme-1.0.min.css" rel="stylesheet" type="text/css"/>
 <link href="jquery.mobile.structure-1.0.min.css" rel="stylesheet" type="text/css"/>
 <script src="jquery-1.6.4.min.js" type="text/javascript"></script>
-<script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>-->
+<script src="jquery.mobile-1.0.min.js" type="text/javascript"></script>
 </head> 
 <body> 
 <div data-role="page" id="page">
